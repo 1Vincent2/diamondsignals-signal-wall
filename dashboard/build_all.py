d1c01b38850dca2a100488d2df38501f48578bdc
Vincent Conti <vconti@mac.com>
2026-05-07 12:47:36 -0400

    Wire Signal Wall V2 into dashboard build

--- dashboard/build_all.py
+++ dashboard/build_all.py
@@ -8,6 +8,7 @@ PREBUILD_COMMANDS = [
 
 MODULES = [
     "dashboard.build_dashboard",
+    "dashboard.build_signal_wall_v2",
     "dashboard.build_call_up_live",
     "dashboard.build_mlb_extraction",
     "dashboard.build_apex_extraction",
